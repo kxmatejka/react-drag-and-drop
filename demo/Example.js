49790f370a83a082bd88b2f6e24c7e3298e473c0
krystof.matejka <krystof.matejka@economia.cz>
2017-12-17 11:41:04 +0100

    Move state from component to parent

--- demo/Example.js
+++ demo/Example.js
@@ -1,27 +1,48 @@
-import React, { Component } from 'react';
-import DragAndDrop from '../src/';
+import React, {Component} from 'react'
+import DragAndDrop from '../src/'
 
 export default class Example extends Component {
-  constructor (props) {
+  constructor(props) {
     super(props)
-    /*this.state = {
+    this.state = {
       files: []
     }
 
-    this.handleChangeFile = this.handleChangeFile.bind(this)*/
+    this.handleChangeFile = this.handleChangeFile.bind(this)
   }
 
-  /*handleChangeFile (event) {
+  handleChangeFile(files) {
     this.setState({
-      files: event.target.files
+      files: files
     })
-  }*/
+  }
 
-  render () {
+  renderFileDetail(file, key) {
+    return (
+      <li key={key}>
+        <strong>{file.name}</strong> ({file.type}, {file.size}B)
+      </li>
+    )
+  }
+
+  render() {
+    const fileList = [...this.state.files].map((file, key) => {
+      return this.renderFileDetail(file, key)
+    })
+
     return (
       <div>
-        <h1>React drag and drop</h1>
-        <DragAndDrop />
+        <div id={"header"}>
+          <h1>React drag and drop</h1>
+        </div>
+        <DragAndDrop label={"Drop files here"}
+                     handler={this.handleChangeFile}
+                     className={"dropzone"}/>
+        <output>
+          <ul>
+            {fileList}
+          </ul>
+        </output>
       </div>
     )
   }
--- src/index.js
+++ src/index.js
@@ -1,52 +1,43 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react'
 
 export default class DragAndDrop extends Component {
-  constructor (props) {
+  constructor(props) {
     super(props)
-
-    this.state = {
-      files: []
-    }
   }
 
-  componentDidMount () {
+  componentDidMount() {
     this.dropZone.addEventListener('dragover', this.handleDragOver.bind(this))
     this.dropZone.addEventListener('drop', this.handleDrop.bind(this))
   }
 
-  componentWillUnmount () {
+  componentWillUnmount() {
     this.dropZone.removeAllListeners()
   }
 
-  handleDragOver (event) {
+  handleDragOver(event) {
     event.preventDefault()
     event.dataTransfer.dropEffect = 'copy'
   }
 
-  handleDrop (event) {
+  handleDrop(event) {
     event.preventDefault()
-    const files = event.dataTransfer.files
-    this.setState({
-      files: files
-    })
+    this.props.handler(event.dataTransfer.files)
   }
 
-  render () {
-    const fileList = [...this.state.files].map((file, index) => {
-      return (<li key={index}>{file.name}</li>)
-    })
+  render() {
+    const {
+      label,
+      style,
+      className
+    } = this.props
 
     return (
-      <fieldset>
-        <div ref={dropZone => {this.dropZone = dropZone}} style={{height: '100px', background: 'blue'}}>
-          Drop files here
-        </div>
-        <output>
-          <ul>
-            {fileList}
-          </ul>
-        </output>
-      </fieldset>
+      <div ref={dropZone => {
+        this.dropZone = dropZone
+      }}
+           style={style} className={className}>
+        <label>{label}</label>
+      </div>
     )
   }
 }
